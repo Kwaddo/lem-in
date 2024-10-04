7643c90403660918a06b3f81ce2571ecc9788e38
mibrahim <moatazusama9@gmail.com>
2024-10-05 01:03:02 +0300

    :

--- lem-in/parse.go
+++ lem-in/parse.go
@@ -7,6 +7,26 @@ import (
 	"strings"
 )
 
+/*
+The idea of the parse is a checker, to see if there are any conflicting connections, any wrong values, any repetitions, and any formats that completely go against
+what should follow through.
+*/
+
+/*
+	Here in parsing we will get the ants number and all the room and also the each room and what rooms its connected to, the connections will be saved in graph struct
+	as well as the start room and the end room. as for the rooms they will be saved in the Room struct saving the name and the x and y values of that room.
+
+	of course we should check if the file extension is a text file or not, then we take the data from the filename given in the parameter. Then we try to validate the following:
+		1. Number of ants can't be zero of above the limit [150,000]
+		2. We catch the [##start] and [##end] flags to know the start and end Rooms
+		3. We catch the Room connection by seeing if the string has a [ - ] sign in it. and saving the results in graph.nodes
+		4. if it catches a comment it continues to the next iteration and ignores it
+		5. anything other than that must be an initialization for a room, then we call the ParseRoom Function to parse the room and get the name and the position of it
+		6. if there is any errors it appends it to allErrors array and returns it and all the catched errors will be printied in the main function
+
+	And of course we had to make the error messages as specific as possible so the user can know exactly where the error is in the input file and fix it.
+*/
+
 type Graph struct {
 	nodes map[string][]string
 	start string
@@ -18,17 +38,17 @@ type Room struct {
 	x, y int
 }
 
-func (graph *Graph) ParseInput(filename string) (int, []Room, error) {
+func (graph *Graph) ParseInput(filename string) (int, []Room, []error) {
 	if !(strings.HasSuffix(filename, ".txt")) {
-		return 0, []Room{}, fmt.Errorf("ERROR: Invalid File Extension, Should be [.txt] file")
+		return 0, []Room{}, []error{fmt.Errorf("ERROR: Invalid File Extension, Should be [.txt] file")}
 	}
 	byteData, err := os.ReadFile(filename)
 	if err != nil {
-		return 0, []Room{}, fmt.Errorf("ERROR: File Not Found, Please enter a valid file name")
+		return 0, []Room{}, []error{fmt.Errorf("ERROR: File Not Found, Please enter a valid file name")}
 	}
-	lines := strings.Split(string(byteData), "\n")
+	lines := strings.Split(string(byteData), "\r\n")
 	if len(lines) < 6 {
-		return 0, []Room{}, fmt.Errorf("ERROR: Invalid input, Not enough input")
+		return 0, []Room{}, []error{fmt.Errorf("ERROR: Invalid input, Not enough input")}
 	}
 	start := false
 	end := false
@@ -38,19 +58,23 @@ func (graph *Graph) ParseInput(filename string) (int, []Room, error) {
 	graph.nodes = make(map[string][]string)
 	var ants int
 	var index int
+	var allErrors []error
 	for i := 0; i < len(lines); i++ {
 		line := i + 1
 		if lines[i] != "" {
 			ants, err = strconv.Atoi(lines[i])
 			if err != nil {
-				return 0, []Room{}, fmt.Errorf("ERROR: Invalid input, Enter number of ants")
+				allErrors = append(allErrors, fmt.Errorf("ERROR: Invalid input, Enter number of ants"))
+				break
 			}
 
 			index = i + 1
 
 			if ants > 150000 || ants < 1 {
-				return 0, []Room{}, fmt.Errorf("ERROR AT LINE %v: Invalid input, Enter number of ants [ 1 - 150000 ]", line)
+				allErrors = append(allErrors, fmt.Errorf("ERROR AT LINE %v: Invalid input, Enter number of ants [ 1 - 150000 ]", line))
+				break
 			}
+			
 			break
 		}
 	}
@@ -89,11 +113,11 @@ func (graph *Graph) ParseInput(filename string) (int, []Room, error) {
 					}
 					graph.end = endRoomArr[0]
 				} else if lines[i] == "##start" && start {
-					return 0, []Room{}, fmt.Errorf("ERROR AT LINE %v: Invalid Input, Redundant ##start flag", line)
+					allErrors = append(allErrors, fmt.Errorf("ERROR AT LINE %v: Invalid Input, Redundant ##start flag", line))
 				} else if lines[i] == "##end" && end {
-					return 0, []Room{}, fmt.Errorf("ERROR AT LINE %v: Invalid Input, Redundant ##end flag", line)
+					allErrors = append(allErrors, fmt.Errorf("ERROR AT LINE %v: Invalid Input, Redundant ##end flag", line))
 				} else {
-					return 0, []Room{}, fmt.Errorf("ERROR AT LINE %v: Invalid Input, Wrong flag entered", line)
+					allErrors = append(allErrors, fmt.Errorf("ERROR AT LINE %v: Invalid Input, Wrong flag entered", line))
 				}
 				continue
 			}
@@ -103,42 +127,50 @@ func (graph *Graph) ParseInput(filename string) (int, []Room, error) {
 			negChecker := strings.Split(lines[i], " ")
 			if len(negChecker) > 1 {
 				if _, err := strconv.Atoi(negChecker[1]); err != nil {
-					return 0, []Room{}, fmt.Errorf("ERROR AT LINE %v: Invalid input, Spaces between connections input", line)
+					allErrors = append(allErrors, fmt.Errorf("ERROR AT LINE %v: Invalid input, Spaces between connections input", line))
 				} else {
 					x, err := strconv.Atoi(negChecker[1])
 					if err != nil {
-						return 0, []Room{}, fmt.Errorf("ERROR AT LINE %v: Invalid X value input", line)
+						allErrors = append(allErrors, fmt.Errorf("ERROR AT LINE %v: Invalid X value input", line))
+						continue
 					}
 					y, err := strconv.Atoi(negChecker[2])
 					if err != nil {
-						return 0, []Room{}, fmt.Errorf("ERROR AT LINE %v: Invalid Y value input", line)
+						allErrors = append(allErrors, fmt.Errorf("ERROR AT LINE %v: Invalid Y value input", line))
+						continue
 					}
 
 					if x < 0 && y < 0 {
-						return 0, []Room{}, fmt.Errorf("ERROR AT LINE %v: Invalid input, Negative X [AND] Y values", line)
+						allErrors = append(allErrors, fmt.Errorf("ERROR AT LINE %v: Invalid input, Negative X [AND] Y values", line))
+						continue
 					} else if x < 0 {
-						return 0, []Room{}, fmt.Errorf("ERROR AT LINE %v: Invalid input, Negative X value", line)
+						allErrors = append(allErrors, fmt.Errorf("ERROR AT LINE %v: Invalid input, Negative X value", line))
+						continue
 					} else if y < 0 {
-						return 0, []Room{}, fmt.Errorf("ERROR AT LINE %v: Invalid input, Negative Y value", line)
+						allErrors = append(allErrors, fmt.Errorf("ERROR AT LINE %v: Invalid input, Negative Y value", line))
+						continue
 					}
 				}
 			}
 			parts := strings.Split(lines[i], "-")
 			if len(parts) > 2 {
-				return 0, []Room{}, fmt.Errorf("ERROR AT LINE %v: Invalid input, Only two rooms can be connected", line)
+				allErrors = append(allErrors, fmt.Errorf("ERROR AT LINE %v: Invalid input, Only two rooms can be connected", line))
+				continue
 			}
 
 			if parts[0] == parts[1] {
-				return 0, []Room{}, fmt.Errorf("ERROR AT LINE %v: Invalid input, Room is connected to it self", line)
+				allErrors = append(allErrors, fmt.Errorf("ERROR AT LINE %v: Invalid input, Room is connected to it self", line))
+				continue
 			}
 
 			graph.nodes[parts[0]] = append(graph.nodes[parts[0]], parts[1])
 			graph.nodes[parts[1]] = append(graph.nodes[parts[1]], parts[0])
-			for _, arr := range graph.nodes {
-				for j := 0; j < len(arr); j++ {
-					for k := j + 1; k < len(arr); k++ {
-						if arr[j] == arr[k] {
-							return 0, []Room{}, fmt.Errorf("ERROR AT LINE %v: Invalid input, Same connection appeared more than once", line)
+
+			for _, conn := range graph.nodes {
+				for j := 0; j < len(conn); j++ {
+					for k := j + 1; k < len(conn); k++ {
+						if conn[j] == conn[k] {
+							return 0, []Room{}, []error{fmt.Errorf("ERROR AT LINE %v: Invalid input, Same connection appeared more than once", line)}
 						}
 					}
 				}
@@ -146,7 +178,7 @@ func (graph *Graph) ParseInput(filename string) (int, []Room, error) {
 		} else {
 			room, err := ParseRoom(lines[i])
 			if err != nil {
-				return 0, []Room{}, err
+				allErrors = append(allErrors, err)
 			}
 			rooms = append(rooms, room)
 		}
@@ -164,7 +196,8 @@ func (graph *Graph) ParseInput(filename string) (int, []Room, error) {
 			}
 		}
 		if !found {
-			return 0, []Room{}, fmt.Errorf("ERROR: Invalid input, Room %v is Unconnected", unconnected)
+			allErrors = append(allErrors, fmt.Errorf("ERROR: Invalid input, Room %v is Unconnected", unconnected))
+			continue
 		}
 	}
 	notEx := ""
@@ -179,24 +212,34 @@ func (graph *Graph) ParseInput(filename string) (int, []Room, error) {
 			}
 		}
 		if !found {
-			return 0, []Room{}, fmt.Errorf("ERROR: Invalid input, Room [%v] connected and does not exist", notEx)
+			allErrors = append(allErrors, fmt.Errorf("ERROR: Invalid input, Room [%v] connected and does not exist", notEx))
+			continue
 		}
 	}
+
 	for i := 0; i < len(rooms); i++ {
 		for j := i + 1; j < len(rooms); j++ {
 			if rooms[i].x == rooms[j].x && rooms[i].y == rooms[j].y {
-				return 0, []Room{}, fmt.Errorf("ERROR: Invalid input, Rooms %v [AND] %v have the same X and Y values", rooms[i], rooms[j])
+				allErrors = append(allErrors, fmt.Errorf("ERROR: Invalid input, Rooms %v [AND] %v have the same X and Y values", rooms[i], rooms[j]))
 			} else if rooms[i].name == rooms[j].name {
-				return 0, []Room{}, fmt.Errorf("ERROR: Invalid input, Rooms %v [AND] %v have the same name", rooms[i], rooms[j])
+				allErrors = append(allErrors, fmt.Errorf("ERROR: Invalid input, Rooms %v [AND] %v have the same name", rooms[i], rooms[j]))
 			}
 		}
 	}
-	if !start {
-		return 0, []Room{}, fmt.Errorf("ERROR: Invalid input, ##start is missing")
+	if !start && !end {
+		allErrors = append(allErrors, fmt.Errorf("ERROR: Invalid input, ##start and ##end flags are missing"))
+	} else if !start {
+		allErrors = append(allErrors, fmt.Errorf("ERROR: Invalid input, ##start is missing"))
 	} else if !end {
-		return 0, []Room{}, fmt.Errorf("ERROR: Invalid input, ##end is missing")
+		allErrors = append(allErrors, fmt.Errorf("ERROR: Invalid input, ##end is missing"))
 	}
-	return ants, rooms, nil
+
+	if len(allErrors) == 0 {
+		return ants, rooms, nil
+	} else {
+		return 0, []Room{}, allErrors
+	}
+
 }
 
 func ParseRoom(line string) (Room, error) {
--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 )
 
+// Where it is all coming together.
+
 func main() {
 	var graph li.Graph
 	args := os.Args[1:]
@@ -13,11 +15,14 @@ func main() {
 		fmt.Print("\nERROR: Invalid number of arguments\n\nUSAGE: go run main.go [INPUT FILENAME]\n")
 		return
 	}
-	ants, rooms, err := graph.ParseInput(args[0])
-	if err != nil {
-		fmt.Println(err)
+	ants, rooms, errs := graph.ParseInput(args[0])
+	if errs != nil {
+		for _, err := range errs {
+			fmt.Println(err)
+		}
 		return
 	}
 	paths := graph.FindPaths(rooms)
+	
 	li.MoveAnts(ants, paths)
 }
